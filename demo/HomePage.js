0d6cbec3a904cb9895b20cf96eeb1623b5e87299
BlendAndroid <1059152860@qq.com>
2023-01-05 15:09:38 +0800

    createStackNavigator的使用

--- demo/HomePage.js
+++ demo/HomePage.js
@@ -1,37 +1,58 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default class HomePage extends React.Component {
 
     render() {
         const { navigation } = this.props;
         return (
-          <View style={styles.container}>
-            <Button
-              title={"FlatListDemo"}
-              onPress={() => {
-                navigation.navigate("FlatListDemo")
-              }}
-            ></Button>
-    
-            <Button
-              title={"SectionListDemo"}
-              onPress={() => {
-                navigation.navigate("SectionListDemo")
-              }}
-            />
-          </View>
+            <View style={styles.container}>
+                <Button
+                    title={"FlatListDemo"}
+                    onPress={() => {
+                        navigation.navigate("FlatListDemo")
+                    }}
+                ></Button>
+
+                <Button
+                    title={"SectionListDemo"}
+                    onPress={() => {
+                        navigation.navigate("SectionListDemo")
+                    }}
+                />
+
+                <Button
+                    title={"Page1"}
+                    onPress={() => {
+                        navigation.navigate("Page1", { name: "动态的" })
+                    }}
+                />
+
+                <Button
+                    title={"Page2"}
+                    onPress={() => {
+                        navigation.navigate("Page2")
+                    }}
+                />
+
+                <Button
+                    title={"Page3"}
+                    onPress={() => {
+                        navigation.navigate("Page3", { name: "Page3的界面" })
+                    }}
+                />
+            </View>
         )
-      }
+    }
 
 }
 
 const styles = StyleSheet.create({
 
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
     },
-  });
+});
--- demo/navigators/AppNavigators.js
+++ demo/navigators/AppNavigators.js
@@ -2,7 +2,12 @@ import React from "react"
 import { createStackNavigator } from 'react-navigation-stack'
 import FlatListDemo from '../FlatListDemo';
 import SectionListDemo from '../SectionListDemo';
-import HomePage from '../HomePage'
+import HomePage from '../HomePage';
+import Page1 from "../Page1";
+import Page2 from "../Page2";
+import Page3 from "../Page3";
+import {Button, Text, ScrollView} from 'react-native';
+
 
 export const AppStackNavigator = createStackNavigator(
     {
@@ -23,6 +28,40 @@ export const AppStackNavigator = createStackNavigator(
             navigationOptions: {
                 title: 'SectionListDemo'
             }
-        }
+        },
+        Page1: {
+            screen: Page1,
+            //从props中取出navigation，所以这里要用{}解构出来
+            navigationOptions: ({ navigation }) => (
+                {
+                    title: `${navigation.state.params && navigation.state.params.name}页面名`//动态设置navigationOptions
+                }
+            )
+        },
+        Page2: {
+            screen: Page2,
+            navigationOptions: {
+                title: "Page2",
+            }
+        },
+        Page3: {
+            screen: Page3,
+            navigationOptions: (props) => {
+                const { navigation } = props;
+                const { state, setParams } = navigation;
+                const { params = {} } = state;
+                return {
+                    title: params.name ? params.name : "This is Page3",
+                    headerRight: (
+                        <Button
+                            title={params.mode === 'edit' ? '保存' : "编辑"}
+                            onPress={() => {
+                                setParams({ mode: params.mode === 'edit' ? "" : 'edit' })
+                            }}
+                        />
+                    )
+                }
+            }
+        },
     }
 );
